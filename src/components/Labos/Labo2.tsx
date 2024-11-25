4551e07db17892979b89b6ca321078f6dd53daff
Hamzaach <hamza.rotf@gmail.com>
2024-11-25 01:08:00 +0100

    labo2.2

--- src/components/Labos/Labo2.tsx
+++ src/components/Labos/Labo2.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import "../../css/Labos.css";
 
 interface HeaderProps {
   title: string;
@@ -41,6 +42,90 @@ const Footer = ({ copy, year }: FooterProps) => {
   );
 };
 
+interface HeaderProps2 {
+  username: string;
+  time: string;
+}
+
+const Header2 = ({ username, time }: HeaderProps2) => {
+  return (
+      <div className={"header"}>
+        <div className={"options"}>
+          <i className="fa fa-chevron-down"></i>
+        </div>
+        <img className={"co_logo"} src="https://loremflickr.com/40/40" />
+        <div className={"co_name"}>
+          <a href="#">{username}</a>
+        </div>
+        <div className={"time"}>
+          <a href="#">{time}</a> · <i className="fa fa-globe"></i>
+        </div>
+      </div>
+  );
+};
+
+interface DescProps {
+  text: string;
+}
+
+const Description = ({ text }: DescProps) => {
+  return (
+      <div className={"content"}>
+        <p>{text}</p>
+      </div>
+  );
+};
+
+interface ContentProps {
+  title: string;
+  subtitle: string;
+  link: string;
+}
+
+const Content = ({ title, subtitle, link }: ContentProps) => {
+  return (
+      <div className={"reference"}>
+        <img
+            className={"reference_thumb"}
+            src="https://loremflickr.com/476/250"
+        />
+        <div className={"reference_content"}>
+          <div className={"reference_title"}>{title}</div>
+          <div className={"reference_subtitle"}>{subtitle}</div>
+          <div className={"reference_font"}>{link}</div>
+        </div>
+      </div>
+  );
+};
+
+interface ActionsProps {
+  thumbs: string;
+  meow: string;
+  share: string;
+}
+
+const Actions = ({ thumbs, meow, share }: ActionsProps) => {
+  return (
+      <div className={"social"}>
+        <div className={"social_content"}></div>
+        <div className={"social_buttons"}>
+        <span>
+          <i className="fa fa-thumbs-up"></i>
+          {thumbs}
+        </span>
+          <span>
+          <i className="fa fa-comment"></i>
+            {meow}
+        </span>
+          <span>
+          <i className="fa fa-share"></i>
+            {share}
+        </span>
+        </div>
+      </div>
+  );
+};
+
 const Labo2 = () => {
   let items: string[] = ["item 1", "item 2", "item 3"];
 
@@ -55,6 +140,27 @@ const Labo2 = () => {
           <Header title="Labo 2.1" subtitle="Basis Componenten" />
           <List items={items} />
           <Footer copy="Hamza Chtaibi" year={2024} />
+        </div>
+        <div>
+          <Header title="Labo 2.2" subtitle="" />
+            <div className={"f_card"}>
+              <Header2 username="Purrfectly Pawsome" time="Just 9 lives ago" />
+
+              <Description text="Do you know why cats are excellent programmers? Because they always purr-fect their code! 😸" />
+
+              <Content
+                  title="The Ultimate Guide to Napping Like a Cat | CatNapTips"
+                  subtitle="Learn the art of napping, feline style! Just follow these simple tips."
+                  link="catnaptips.com"
+              />
+
+              <Actions
+                  thumbs="Paws Up!"
+                  meow="Meow-back"
+                  share="Share the Cat-titude"
+              />
+            </div>
+
         </div>
       </div>
     </>
